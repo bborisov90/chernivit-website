a7409b97893a65c70a4aafe59c56b38284a080b3
Boris Borisov <bborisov@Boriss-MacBook-Air.local>
2026-06-29 01:19:08 +0300

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://chernivit.com",
+  integrations: [sitemap()],
 });
